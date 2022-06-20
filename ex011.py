91444613ce522731cb33ed25e13a30137702ecac
Fabio Morais de Andrade <fabio.adr12@gmail.com>
2022-06-20 18:10:21 -0300

    Exercicio 11

---
+++ ex011.py
@@ -0,0 +1,5 @@
+l = float(input('Qual a largura da parede em metros? '))
+h = float(input('Qual a altura da parede em metros? '))
+a = l*h
+t = a/2
+print('A área da parede é {} metros quadrados. \nA quantidade de tinta necessária para pintar a parede é {} l.'.format(a, t))
